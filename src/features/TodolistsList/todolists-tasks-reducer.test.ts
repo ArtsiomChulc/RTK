c140cd810e269015a005d57e91bdd88834dbb969
ArtsiomChulc <artemorsha@mail.ru>
2023-07-11 16:15:41 +0300

    set prettier

--- src/features/TodolistsList/todolists-tasks-reducer.test.ts
+++ src/features/TodolistsList/todolists-tasks-reducer.test.ts
@@ -1,22 +1,22 @@
-import {addTodolistAC, TodolistDomainType, todolistsReducer} from './todolists-reducer'
-import {tasksReducer, TasksStateType} from './tasks-reducer'
-import {TodolistType} from '../../api/todolists-api'
+import { addTodolistAC, TodolistDomainType, todolistsReducer } from "./todolists-reducer";
+import { tasksReducer, TasksStateType } from "./tasks-reducer";
+import { TodolistType } from "../../api/todolists-api";
 
-test('ids should be equals', () => {
+test("ids should be equals", () => {
     const startTasksState: TasksStateType = {};
     const startTodolistsState: Array<TodolistDomainType> = [];
 
     let todolist: TodolistType = {
-        title: 'new todolist',
-        id: 'any id',
-        addedDate: '',
-        order: 0
-    }
+        title: "new todolist",
+        id: "any id",
+        addedDate: "",
+        order: 0,
+    };
 
     const action = addTodolistAC(todolist);
 
-    const endTasksState = tasksReducer(startTasksState, action)
-    const endTodolistsState = todolistsReducer(startTodolistsState, action)
+    const endTasksState = tasksReducer(startTasksState, action);
+    const endTodolistsState = todolistsReducer(startTodolistsState, action);
 
     const keys = Object.keys(endTasksState);
     const idFromTasks = keys[0];
