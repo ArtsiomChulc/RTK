d5b39e9e73c5603594ebb9606615b84397edda82
ArtsiomChulc <artemorsha@mail.ru>
2023-07-11 19:27:36 +0300

    create store with redux-toolkit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
-import { tasksReducer } from "../features/TodolistsList/tasks-reducer";
-import { todolistsReducer } from "../features/TodolistsList/todolists-reducer";
-import { AnyAction, applyMiddleware, combineReducers, createStore } from "redux";
-import thunkMiddleware, { ThunkAction, ThunkDispatch } from "redux-thunk";
+import { tasksReducer } from "features/TodolistsList/tasks-reducer";
+import { todolistsReducer } from "features/TodolistsList/todolists-reducer";
+import { AnyAction, combineReducers } from "redux";
+import { ThunkAction, ThunkDispatch } from "redux-thunk";
 import { appReducer } from "./app-reducer";
-import { authReducer } from "../features/Login/auth-reducer";
+import { authReducer } from "features/Login/auth-reducer";
+import { configureStore } from "@reduxjs/toolkit";
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -13,8 +14,13 @@ const rootReducer = combineReducers({
     app: appReducer,
     auth: authReducer,
 });
+
+// непосредственно создаём store / redux-toolkit
+export const store = configureStore({
+    reducer: rootReducer,
+});
 // непосредственно создаём store
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+// export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>;
 
